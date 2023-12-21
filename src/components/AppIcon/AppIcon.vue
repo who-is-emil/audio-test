33c0cfb7e0cb391a6d0192ec15fcd6278c5bc1cc
emiltukhvatullin <e.tuhvatullin@uplab.ru>
2023-12-21 17:54:33 +0500

    init

---
+++ src/components/AppIcon/AppIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <span class="app-icon" :class="className">
+   <inline-svg :src="src"/>
+  </span>
+</template>
+
+<script>
+import InlineSvg from 'vue-inline-svg';
+
+export default {
+  name: 'AppIcon',
+  // components: { AppPlus },
+  // components: { IconPlus },
+  components: {
+    InlineSvg,
+  },
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+    className: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    src() {
+      // eslint-disable-next-line global-require,import/no-dynamic-require
+      return require(`@/assets/icons/${this.name}`);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "AppIcon";
+</style>
