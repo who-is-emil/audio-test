10a094ba72b2953b9bd1a1120e4ce93012cb1a3d
emiltukhvatullin <e.tuhvatullin@uplab.ru>
2024-01-09 16:29:39 +0500

    подключил базовые стили

---
+++ src/components/AppButton/AppButton.vue
@@ -0,0 +1,68 @@
+<template>
+  <a
+    v-if="href"
+    :href="href"
+    :class="['app-button', theme, size]"
+    :title="title"
+    :target="target"
+  >
+    <span class="app-button__text">{{ text }}</span>
+  </a>
+  <button
+    v-else
+    :type="type"
+    :class="['app-button', theme, size]"
+    :title="title"
+    @click="click"
+  >
+    <span class="app-button__text">{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    type() {
+      return this.data.type || 'button';
+    },
+    href() {
+      return this.data.href || false;
+    },
+    target() {
+      return this.data.target || false;
+    },
+    text() {
+      return this.data.text || '';
+    },
+    title() {
+      return this.data.title || this.text;
+    },
+    theme() {
+      return this.data.theme
+        ? `app-button--${this.data.theme}`
+        : '';
+    },
+    size() {
+      return this.data.size
+        ? `app-button--${this.data.size}`
+        : '';
+    },
+  },
+  methods: {
+    click() {
+      this.$emit('click');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+//@import "AppButton";
+</style>
